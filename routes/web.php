9f43347a51467400d30c1e29d73569acb98456d5
Esaiy <93sultanbaney@gmail.com>
2021-12-12 08:58:06 +0700

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers as Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [Controller\DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
