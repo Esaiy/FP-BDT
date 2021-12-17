dbc5eaa618bc7c5f44a4d5df8cd0abcb9c99f310
Esaiy <93sultanbaney@gmail.com>
2021-12-17 08:36:11 +0700

    redis

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -25,6 +25,7 @@ class FrontEndController extends Controller
         
         if ($redisGet->get('page:articles:index')) {
             $articlesRedis = json_decode($redisGet->get('articles'), true);
+            dd($categories, $redisGet->get('page:articles:index'));
             $articles = new LengthAwarePaginator($articlesRedis['data'], $articlesRedis['total'], $articlesRedis['per_page'], $articlesRedis['current_page']);
         } else {
             $articles = Article::with(['category','author'])
